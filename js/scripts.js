d2731c8064fc7f1ebe42b9c2873f345143286e62
KevinMokoro <klosvoke1@gmail.com>
2018-12-03 11:03:22 +0300

    add changes to js and index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
-function triangle () {
-// return type of tringle //
-var sides =[]
-var a = parseInt(document.getElementById('a').value);
-var b = parseInt(document.getElementById('b').value);
-var c = parseInt(document.getElementById('c').value);
-var display = document.querySelector("#display");
-sides.push(a);
-sides.push(b);
-sides.push(c);
-if (a===b && b===c && c===a){
-display.innerHTML="equilateral triangle";
-}
-else if ((a+b===c || b+c===a || a+c===b) || (a+b<c || b+c<a || a+c<b)){
-display.innerHTML="Not a triangle";
-}
-else if (a===b || b===c || a===c){
-display.innerHTML="isosceles triangle";
-}
-else if ((a+b>c || a+c>b || b+c>a)&&(a!==b || b!==c || c!==a)){
-display.innerHTML="scalene triangle";
-}
+function triangle() {
+  // return type of tringle //
+  var sides = []
+  var a = parseInt(document.getElementById('a').value);
+  var b = parseInt(document.getElementById('b').value);
+  var c = parseInt(document.getElementById('c').value);
+  var display = document.querySelector("#display");
+  sides.push(a);
+  sides.push(b);
+  sides.push(c);
+  if (a === b && b === c && c === a) {
+    display.innerHTML = "equilateral triangle";
+  } else if ((a + b === c || b + c === a || a + c === b) || (a + b < c || b + c < a || a + c < b)) {
+    display.innerHTML = "Not a triangle";
+  } else if (a === b || b === c || a === c) {
+    display.innerHTML = "isosceles triangle";
+  } else if ((a + b > c || a + c > b || b + c > a) && (a !== b || b !== c || c !== a)) {
+    display.innerHTML = "scalene triangle";
+  }
 }
