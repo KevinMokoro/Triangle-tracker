4f240eb9505a32b0280f309a12cb5a0f891cb33e
KevinMokoro <klosvoke1@gmail.com>
2018-12-03 15:16:02 +0300

    add few chsnges to css js and index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 function triangle() {
   // return type of tringle //
-  var sides = []
+  
   var a = parseInt(document.getElementById('a').value);
   var b = parseInt(document.getElementById('b').value);
   var c = parseInt(document.getElementById('c').value);
   var display = document.getElementById("display");
-  sides.push(a);
-  sides.push(b);
-  sides.push(c);
+
+
   if (a === b && b === c && c === a) {
     display.innerHTML = "equilateral triangle";
   } else if ((a + b === c || b + c === a || a + c === b) || (a + b < c || b + c < a || a + c < b)) {
