eafb97cec31b5d5e2881e70cad0d252ef6c54a36
KevinMokoro <klosvoke1@gmail.com>
2018-12-05 11:41:55 +0300

    modifications to README CSS JS and index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
 function triangle() {
-  // return type of tringle //
-  var sides = []
   var a = parseInt(document.getElementById('a').value);
   var b = parseInt(document.getElementById('b').value);
   var c = parseInt(document.getElementById('c').value);
-  var display = document.getElementById("display");
+  if (a > 0 && b > 0 && c > 0) {
+    if (a === b && b === c && c === a) {
+      alert("equilateral triangle");
+    } else if ((a + b === c || b + c === a || a + c === b) || (a + b < c || b + c < a || a + c < b)) {
+      alert("Not a triangle");
+    } else if (a === b || b === c || a === c) {
+      alert("isosceles triangle");
 
-  sides.push(a);
-  sides.push(b);
-  sides.push(c);
-  if (a === b && b === c && c === a) {
-    display.innerHTML = "equilateral triangle";
-  } else if ((a + b === c || b + c === a || a + c === b) || (a + b < c || b + c < a || a + c < b)) {
-    display.innerHTML = "Not a triangle";
-  } else if (a === b || b === c || a === c) {
-    display.innerHTML = "isosceles triangle";
-
-  } else if ((a + b > c || a + c > b || b + c > a) && (a !== b || b !== c || c !== a)) {
-    display.innerHTML = "scalene triangle";
+    } else if ((a + b > c || a + c > b || b + c > a) && (a !== b || b !== c || c !== a)) {
+      alert("scalene triangle");
+    }
   } else {
-    display.innerHTML = "no values entered";
+    alert("no values entered");
   }
+}
+
+function refresh() {
+  location.reload();
 }
